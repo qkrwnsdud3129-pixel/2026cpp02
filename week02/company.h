c4e886626cc4bf3b2d4a2566f857030311756ac2
qkrwnsdud3129-pixel <qkrwnsdud3129@gmail.com>
2026-09-10 16:09:23 +0900

    Receipt Project v0.2

--- week02/company.h
+++ week02/company.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Company
+{
+private:
+    string name;
+    string tel;
+public:
+    Company(string name, string tel);
+    ~Company();
+    void print() const;
+};
